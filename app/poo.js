64fcad603ece1d8662904a3c45bdf1239a5a07fa
Mati <matirossi93@gmail.com>
2023-08-29 09:54:52 +0200

    ejerc-java-poo

---
+++ app/poo.js
@@ -0,0 +1,38 @@
+
+// EJERCICIO NUMERO 2 
+
+/* let cuenta = {
+    titular: "Alex",
+    saldo: 0,
+    modelo: 2023,
+    ingresar: function(dinero){
+    if(dinero > 0) {
+
+        this.saldo += dinero;
+        console.log(`Se han ingresado ${dinero} euros en la cuenta`)
+    } else {
+        console.log("La cantidad ingresada debe ser mayor que 0")
+    }
+   },
+
+   extraer: function(dinero) {
+
+    if(dinero < this.saldo) {
+
+        this.saldo -= dinero;
+        console.log(`Se han retirado ${dinero} euros de la cuenta`)
+
+    } else {
+        console.log("No tienes suficiente dinero en la cuenta");
+    }
+   },
+
+   informar: function(){
+
+        console.log(`Cuenta de ${this.titular}, tiene un saldo de ${this.saldo}`);
+
+
+   },
+   
+}
+*/
