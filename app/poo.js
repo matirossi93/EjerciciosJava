bd488862c3a2a7a17dfc1369061594ddb39c9d11
Mati <matirossi93@gmail.com>
2023-08-29 10:48:53 +0200

    ejercicios

--- app/poo.js
+++ app/poo.js
@@ -36,3 +36,33 @@
    
 }
 */
+
+// EJERCICIO NUMERO 4
+
+/* class Producto {
+
+    constructor(codigo, nombre, precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+
+    }
+
+    imprimeDatos(){
+        console.log(`el codigo es ${this.codigo}`);
+        console.log(`el nombre es ${this.nombre}`);
+        console.log(`el precio es ${this.precio}`);
+    }
+}
+
+const producto1 = new Producto("001", "lapiz", "$150");
+const producto2 = new Producto("002", "lapicera", "$150");
+const producto3 = new Producto("003", "goma", "$150");
+
+const productos = [producto1, producto2, producto3];
+   
+productos.forEach(producto => {
+    producto.imprimeDatos();
+    console.log("-------")
+}) */
+
